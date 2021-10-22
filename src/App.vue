d7aa5d763f4e7aa74a1728fb3b3ac26f59982bbb
janick <janick.perosa97@hotmail.com>
2021-10-22 20:03:14 +0200

    vue js projekt installiert

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <navBar></navBar>
+</template>
+
+<script>
+// @ is an alias to /src
+import NavBar from '@/components/NavBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavBar
+  }
+}
+</script>
+
+<style>
+body {
+  background-color: #1A191C;
+}
+</style>
