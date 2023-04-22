c712d6754b85b46d6ee290775699aea343c819fe
janick <janick.perosa97@hotmail.com>
2023-04-22 21:26:20 +0200

    WIP: Portfolio Redesign

--- src/components/ProgrammiersprachenComponent.vue
+++ src/components/ProgrammiersprachenComponent.vue
@@ -1,71 +1,69 @@
 <template>
-    <!-- Programmiersprachen Section Start-->
-    <p> Hier sind einige Technologien, mit denen ich in letzter Zeit gearbeitet habe:</p>
-   <v-row >
-        <v-col  v-for="item in items" :key="item.id" cols="2">
-            <div class="badge-wrapper">
-                <v-chip class="ma-2" variant="outlined" label :color="item.color">
-                    <v-icon start :icon="item.icon"></v-icon>
-                    {{ item.title }}
-                </v-chip>
-            </div>
-        </v-col>
-    </v-row> 
+  <!-- Programmiersprachen Section Start-->
+  <p> Hier sind einige Technologien, mit denen ich in letzter Zeit gearbeitet habe:</p>
+  <v-container fluid style="margin: 0px; padding: 0px; width: 80%">
+    <v-row>
+      <v-col v-for="item in items" :key="item.id" cols="2">
+        <v-chip class="ma-2" variant="outlined" label :color="item.color">
+          <v-icon start :icon="item.icon"></v-icon>
+          {{ item.title }}
+        </v-chip>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 <script>
 export default {
-    data: () => ({
-        items: [
-            {
-              color: 'info',
-              icon: 'mdi-language-html5',
-              title: 'HTML & CSS',
-            },
-            {
-              color: 'info',
-              icon: 'mdi-language-javascript',
-              title: 'Javascript',
-            },
-            {
-              color: 'info',
-              icon: 'mdi-wordpress',
-              title: 'Wordpress',
-            },
-            {
-              color: 'info',
-              icon: 'mdi-alpha-c-box-outline',
-              title: 'Craft cms',
-            },
-            {
-              color: 'info',
-              icon: 'mdi-language-php',
-              title: 'PHP & MYSQL',
-            },
-            {
-              color: 'info',
-              icon: 'mdi-symfony',
-              title: 'Symfony',
-            },
-            {
-              color: 'info',
-              icon: 'mdi-laravel',
-              title: 'Laravel',
-            },
-            {
-              color: 'info',
-              icon: 'mdi-vuejs',
-              title: 'Vue Js',
-            },
-            {
-              color: 'info',
-              icon: 'mdi-nodejs',
-              title: 'Node Js',
-            },
-        ],
-    })
+  data: () => ({
+    items: [
+      {
+        color: 'info',
+        icon: 'mdi-language-html5',
+        title: 'HTML & CSS',
+      },
+      {
+        color: 'info',
+        icon: 'mdi-language-javascript',
+        title: 'Javascript',
+      },
+      {
+        color: 'info',
+        icon: 'mdi-wordpress',
+        title: 'Wordpress',
+      },
+      {
+        color: 'info',
+        icon: 'mdi-alpha-c-box-outline',
+        title: 'Craft cms',
+      },
+      {
+        color: 'info',
+        icon: 'mdi-language-php',
+        title: 'PHP & MYSQL',
+      },
+      {
+        color: 'info',
+        icon: 'mdi-symfony',
+        title: 'Symfony',
+      },
+      {
+        color: 'info',
+        icon: 'mdi-laravel',
+        title: 'Laravel',
+      },
+      {
+        color: 'info',
+        icon: 'mdi-vuejs',
+        title: 'Vue Js',
+      },
+      {
+        color: 'info',
+        icon: 'mdi-nodejs',
+        title: 'Node Js',
+      },
+    ],
+  })
 }
 
 </script>
-<style>
-
-</style>
+<style></style>
--- src/components/TimeLine.vue
+++ src/components/TimeLine.vue
@@ -1,65 +1,93 @@
 <template>
   <div>
-    <h1>Experience</h1>
+    <h1>Erfahrungen</h1>
     <v-row>
       <v-col>
-        <v-timeline>
-          <v-timeline-item v-for="(item, i) in items" :key="i">
-            <v-card max-width="500" height="auto" class="pa-2">
-              <v-card-title>{{ item.title }}</v-card-title>
-              <v-card-subtitle>
-                <span>{{ item.date }}, {{ item.firma }}</span>
-              </v-card-subtitle>
-              <v-card-text>{{ item.text }}</v-card-text>
-              <v-btn @click="showPDF(item.pdfUrl)" variant="outlined"> Button</v-btn>
-            </v-card>
-          </v-timeline-item>
-        </v-timeline>
+        <v-tabs v-model="tab" align-tabs="center" centered stacked>
+          <v-tab value="tab-1">
+            <v-icon>mdi-briefcase-outline</v-icon>
+            Arbeit
+          </v-tab>
+          <v-tab value="tab-2">
+            <v-icon>mdi-account-school-outline</v-icon>
+            Schule
+          </v-tab>
+        </v-tabs>
+
+        <v-container center>
+          <v-window v-model="tab">
+            <v-window-item value="tab-1">
+              <v-timeline>
+                <v-timeline-item v-for="(work, index) in works" :key="index">
+                  <v-card max-width="500" height="180" class="pa-2">
+                    <v-card-title>{{ work.title }}</v-card-title>
+                    <v-card-subtitle>
+                      <span>{{ work.date }}, {{ work.firma }}</span>
+                    </v-card-subtitle>
+                    <v-card-text>{{ work.text }}</v-card-text>
+                    <!-- <v-btn @click="showPDF(item.pdfUrl)" variant="outlined">Button</v-btn> -->
+                  </v-card>
+                </v-timeline-item>
+              </v-timeline>
+            </v-window-item>
+            <v-window-item value="tab-2">
+              <v-timeline>
+                <v-timeline-item v-for="(experience, index) in experiences" :key="index">
+                  <v-card max-width="auto" height="100" class="pa-2">
+                    <v-card-title>{{ experience.title }}</v-card-title>
+                    <v-card-subtitle>
+                      <span>{{ experience.school }}, {{ experience.date }}</span>
+                    </v-card-subtitle>
+                    <!-- <v-btn @click="showPDF(experience.pdfUrl)" variant="outlined">Button</v-btn> -->
+                  </v-card>
+                </v-timeline-item>
+              </v-timeline>
+            </v-window-item>
+          </v-window>
+        </v-container>
       </v-col>
     </v-row>
   </div>
 </template>
-
 <script>
 export default {
   data: () => ({
-    items: [
+      tab: null,
+      works: [
+        {
+          date: '02.2023 - 04.2023',
+          title: 'Praktikum Webentwickler',
+          firma: 'H2G Internetagentur in Aarau',
+          text: 'Sicherstellen der Kundenaufträgen, Einrichten, Produktion und Überwachung der Produktionsanlag.',
+          pdfUrl: '../assets/Lebenslauf_Janick_Perosa.pdf',
+        },
+        {
+          date: '01.2020 - 06.2020',
+          title: 'Maschinenführer',
+          firma: 'Effingermedien in Kleindöttingen',
+          text: 'Sicherstellen der Kundenaufträgen, Einrichten, Produktion und Überwachung der Produktionsanlag.',
+        },
+        {
+          date: '07.2019 - 11.2019',
+          title: 'Mediendesigner',
+          firma: 'Jobtv medienwerkstatt in Zürich',
+          text: 'Digitalegrafische Konzepte, Screendesign, Logos und Flyer.',
+        },
+      ],
+       experiences: [
       {
-        date: '02.2023 - 04.2023',
-        title: 'Praktikum Webentwickler',
-        firma: 'H2G Internetagentur in Aarau',
-        text: 'Sicherstellen der Kundenaufträgen, Einrichten, Produktion und Überwachung der Produktionsanlag.',
-        pdfUrl: '../assets/Lebenslauf_Janick_Perosa.pdf',
+        title: 'Webdesign & Development',
+        school: 'sae institute',
+        date: '03.2019 - 03.2021'
       },
       {
-        date: '03.2019 - 03.2021',
-        title: 'Webdesgin & Development',
-        firma: 'SAE Institute in Zürich',
-        text: '',
-      },
-      {
-        date: '01.2020 - 06.2020',
-        title: 'Maschinenführer',
-        firma: 'Effingermedien in Kleindöttingen',
-        text: 'Sicherstellen der Kundenaufträgen, Einrichten, Produktion und Überwachung der Produktionsanlag.',
-      },
-      {
-        date: '07.2019 - 11.2019',
-        title: 'Mediendesigner',
-        firma: 'Jobtv medienwerkstatt in Zürich',
-        text: 'Digitalegrafische Konzepte, Screendesign, Logos und Flyer.',
-      },
+        title: 'Printmedienverarbeiter Druckausrüster',
+        school: 'Schule für Gestaltung Bern/Biel',
+        date: '08.2014 - 07.2017'
+      }
     ],
-  }),
-  methods: {
-    showPDF(pdfUrl) {
-      console.log(pdfUrl);
-      window.open(pdfUrl, '_blank')
-    }
-  }
+    }),
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
